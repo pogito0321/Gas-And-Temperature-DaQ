060b28e2330b200810b128a3d4c9b59e724d5374
pogito0321 <jimpauldevera.jdv@gmail.com>
2017-10-17 15:39:12 +0800

    Add files via upload

---
+++ Connection_to_Database.pde
@@ -0,0 +1,57 @@
+import de.bezier.data.sql.*;  
+import processing.serial.*;   
+
+MySQL msql;     
+
+String[] a;
+int end = 10;    
+String serial;   
+Serial port;     
+int x = 0;
+
+void setup() {
+  String user     = "root";
+  String pass     = "";
+  String database = "datalogging";
+  
+  msql = new MySQL( this, "localhost", database, user, pass );
+    
+  port = new Serial(this, Serial.list()[0], 9600); 
+  port.clear(); 
+  serial = port.readStringUntil(end); 
+  serial = null; 
+}
+void draw() 
+{
+  
+  while (port.available() > 0) 
+  { 
+    
+    serial = port.readStringUntil(end);
+  }
+    if (serial != null) 
+    {  
+      a = split(serial, ',');
+      print("Gas: ");
+      print(a[0]);
+      println(" KJ/mol");
+      
+      print("Temp: ");
+      print(a[1]);
+      println(" Celsius");
+      function();
+    }
+}
+
+void function()
+{
+  if ( msql.connect() )
+    {
+        msql.query( "insert into gas_and_temp(Gas, Temperature) values("+a[0]+", "+a[1]+")" );
+    }
+    else
+    {
+       
+    }
+    msql.close(); 
+}
---
+++ TheOneES3_MQ135_LM35.ino
@@ -0,0 +1,67 @@
+float temp;
+int tempPin = A0;
+int sensorThres = 400; 
+
+void setup() 
+{
+  //lcd.begin(16, 2);
+  //lcd.setCursor(0, 0);
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(A1, INPUT);
+  //delay(1000);
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  int analogSensor = analogRead(A1);
+
+ 
+  Serial.print(analogSensor);
+  Serial.print(",");
+  if(analogSensor > sensorThres)
+  {
+    digitalWrite(10, HIGH);
+    digitalWrite(9, LOW);
+    tone(8, 1000, 200);
+  }
+
+  else
+  {
+    digitalWrite(10, LOW);
+    digitalWrite(9, HIGH);
+    noTone(8);
+  }
+  
+ 
+  temp = analogRead(tempPin);
+  temp = (temp/1024) * 310;
+  Serial.println(temp);
+  delay(2000);
+  
+  if(temp <= 37)    
+  {
+     digitalWrite(7, HIGH);
+     digitalWrite(6, LOW);
+     digitalWrite(5, LOW);     
+  }
+  
+  else if(temp >= 37 && temp <= 40) 
+  {
+    digitalWrite(7, LOW);
+    digitalWrite(6, HIGH);
+    digitalWrite(5, LOW);
+  }
+  
+  else if (temp >= 41)
+  {
+    digitalWrite(7, LOW);
+    digitalWrite(6, LOW);
+    digitalWrite(5, HIGH);
+  }
+  
+}
